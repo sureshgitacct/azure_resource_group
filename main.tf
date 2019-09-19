d4da2c68ce376e736fa0b403b8690331760a73b7
Ryan <ryan.fee625@gmail.com>
2019-09-19 14:44:09 -0400

    Create main.tf

---
+++ main.tf
@@ -0,0 +1,11 @@
+provider "azurerm" {
+    subscription_id = "${var.scalr_azurerm_subscription_id}"
+    client_id = "${var.scalr_azurerm_client_id}"
+    client_secret = "${var.scalr_azurerm_client_secret}"
+    tenant_id = "${var.scalr_azurerm_tenant_id}"
+}
+
+resource "azurerm_resource_group" "resource_group" {
+    name     =  var.name
+    location =  var.region
+}
